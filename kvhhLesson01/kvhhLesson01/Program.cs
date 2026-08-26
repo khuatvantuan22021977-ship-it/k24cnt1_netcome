7304d48efcfdc76c4f962c187c01e9a00524239f
khuatvantuan22021977-ship-it <khuatvantuan22021977@gmail.com>
2026-08-26 12:59:03 +0700

    First commit

---
+++ kvhhLesson01/kvhhLesson01/Program.cs
@@ -0,0 +1,15 @@
+namespace kvhhLesson01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello !  khuat van hoang hiep");
+
+            app.Run();
+        }
+    }
+}
