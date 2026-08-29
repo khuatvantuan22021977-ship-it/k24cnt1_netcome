85fe23b3a482e6c451c92d53d532460677b1a0a4
khuatvantuan22021977-ship-it <khuatvantuan22021977@gmail.com>
2026-08-29 22:04:51 +0700

    kvhhLesson04Lab

---
+++ kvhhLesson04Lab/kvhhLesson04Lab/Controllers/kvhhAccountController.cs
@@ -0,0 +1,69 @@
+﻿using kvhhLesson04Lab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kvhhLesson04Lab.Controllers
+{
+    public class kvhhAccountController : Controller
+    {
+        private readonly List<kvhhAccount> kvhhAccounts = new()
+        {
+            new kvhhAccount
+            {
+                Id = 1,
+                Name = "Nguyễn Ngọc Anh",
+                Email = "nguyenngocanh@gmail.com",
+                Phone = "0901234567",
+                Anime = "/imager/1.jpg",
+                Address = "Hà Nội",
+                Bio = "Yêu thích anime và manga",
+                Gender = 0,
+                Birthday = new DateTime(2005, 5, 10)
+            },
+
+            new kvhhAccount
+            {
+                Id = 2,
+                Name = "Trần Thị Mai",
+                Email = "tranthimai@gmail.com",
+                Phone = "0912345678",
+                Anime = "/imager/2.jpg",
+                Address = "Hải Phòng",
+                Bio = "Thích xem anime và đọc truyện",
+                Gender = 0,
+                Birthday = new DateTime(2006, 8, 15)
+            },
+
+            new kvhhAccount
+            {
+                Id = 3,
+                Name = "Lê Khánh Linh",
+                Email = "lekhanhlinh@gmail.com",
+                Phone = "0923456789",
+                Anime = "/imager/3.jpg",
+                Address = "Đà Nẵng",
+                Bio = "Fan của One Piece",
+                Gender = 0,
+                Birthday = new DateTime(2004, 3, 22)
+            },
+
+            new kvhhAccount
+            {
+                Id = 4,
+                Name = "Phạm Ngọc Linh",
+                Email = "phamngoclinh@gmail.com",
+                Phone = "0934567890",
+                Anime = "/imager/4.jpg",
+                Address = "Hồ Chí Minh",
+                Bio = "Yêu thích anime hành động",
+                Gender = 0,
+                Birthday = new DateTime(2005, 11, 5)
+            }
+        };
+
+        public IActionResult kvhhIndex()
+        {
+            ViewBag.kvhhAccounts = kvhhAccounts;
+            return View(kvhhAccounts);
+        }
+    }
+}
---
+++ kvhhLesson04Lab/kvhhLesson04Lab/Models/kvhhAccount.cs
@@ -0,0 +1,18 @@
+﻿namespace kvhhLesson04Lab.Models
+{
+    public class kvhhAccount
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public string Phone  {  get; set; }
+        public string Anime { get; set; }
+
+        public string Address { get; set; }
+
+        public string Bio { get; set; }
+        public int Gender { get; set; }
+        public  DateTime Birthday { get; set; }
+    }
+}
